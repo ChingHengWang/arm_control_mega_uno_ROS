efc584b961a873166cdaed6ced128ca338dcf152
ChingHengWang <qoogood1234@gmail.com>
2015-10-24 19:57:07 +0800

    put code in function for uno

--- pos_control_uno_noROS/pos_control_uno_noROS.ino
+++ pos_control_uno_noROS/pos_control_uno_noROS.ino
@@ -14,90 +14,108 @@
 
 int x=0;
 
-//INTEGER CMD_POS DECODER  (BYTE -> INTEGER)
-char commandArray[3];
-char rS=0;
-byte rH=0;//HIGH BYTE
-byte rL=0;//LOW BYTE
-byte rF=0;
-int cmd0Pos=0; //degree
+int cmdPos=0; //degree
 
 
 //ENC STATE
 int pinAState = 0;int pinAStateOld = 0;int pinBState = 0;int pinBStateOld = 0;
 
-double angle0Pos = 0;
-volatile long encoder0Pos = 0;
+volatile long encoderPos = 0;
 volatile long unknownvalue = 0;
 
 Metro mainTimer = Metro(10);
 
 
 void setup() { 
-  pinMode(MotorPin0, OUTPUT);
-  pinMode(MotorPin1, OUTPUT);
 
-  
-  pinMode(encoder0PinA, INPUT); 
-  digitalWrite(encoder0PinA, HIGH); // turn on pullup resistor
-  pinMode(encoder0PinB, INPUT); 
-  digitalWrite(encoder0PinB, HIGH); // turn on pullup resistor
-  
-  attachInterrupt(0, doEncoder, CHANGE); // encoder pin on interrupt 0 - pin 2
-  attachInterrupt(1, doEncoder, CHANGE);
-  
-  Serial.begin (57600);
-  //Serial.println("start"); // a personal quirk
+    pinMode(MotorPin0, OUTPUT);
+    pinMode(MotorPin1, OUTPUT);
+
+    pinMode(encoder0PinA, INPUT); 
+    digitalWrite(encoder0PinA, HIGH); // turn on pullup resistor
+    pinMode(encoder0PinB, INPUT); 
+    digitalWrite(encoder0PinB, HIGH); // turn on pullup resistor
+
+    attachInterrupt(0, doEncoder, CHANGE); // encoder pin on interrupt 0 - pin 2
+    attachInterrupt(1, doEncoder, CHANGE);
+
+    Serial.begin (57600);
 } 
 
 void loop(){
   if (mainTimer.check() == true) { 
 
-  //GET POS_CMD FROM MEGA
-   
-      rS=(char)Serial.read();
-       if(rS=='{'){
-         Serial.readBytes(commandArray,3);
-         rH=commandArray[0];
-         rL=commandArray[1];
-         rF=commandArray[2];
-         if(rF=='}')         
-           cmd0Pos=(rH<<8)+rL; 
-   // ACTUALLY NOW WE DONT NEED TO SEND ENC TO MEGA        
-	   //Serial.print("  cmd  ");Serial.print(cmd0Pos);
-           //Serial.print("  rS  ");Serial.print(rS);
-           //Serial.print("  rH  ");Serial.print(rH);
-           //Serial.print("  rL  ");Serial.print(rL);
-           //Serial.print("  rF  ");Serial.println(rF);
-         rS=0;
-         
-        for(int i = 0; i < 3; ++i){
-          commandArray[i] = '\0';
-        }
-        
-       }
+    //GET CMD FROM MEGA
+    get_cmd_pos();
 
-
-
-
-    //GET ANGLE
-    angle0Pos=-1*encoder0Pos*0.00694;//(double)360/(64*810*1);
-    //angle0Pos=(double)((double)(360.0/64)/810);
+    //GET ENC
+    double anglePos=get_angle_from_enc();
 
     //CONTROL STRATERGY
-    double cmdPwm=10*(cmd0Pos-angle0Pos);
+    double cmdPwm=10*(cmdPos-anglePos);
  
-    //SEND CMD TO MOTOR
+    //SEND CMD
+    send_cmd_to_motor(cmdPwm);
+
+    /////WRITE ENC to mega
+    /*
+    x=encoderPos;  
+    char xS='{';
+    byte xH =highByte(x);
+    byte xL =lowByte(x);
+    char xF='}'; 
+    Serial.print(xS);Serial.write(xH);Serial.write(xL);Serial.write(xF);
+    */
+    
+    }
+}
+
+
+void get_cmd_pos() {
+
+    //GET POS_CMD FROM MEGA
+
+     char rS=(char)Serial.read();
+     if(rS=='{'){
+     char commandArray[3];
+     Serial.readBytes(commandArray,3);
+     byte rH=commandArray[0];
+     byte rL=commandArray[1];
+     char rF=commandArray[2];
+     if(rF=='}')         
+       cmdPos=(rH<<8)+rL; 
+     // ACTUALLY NOW WE DONT NEED TO SEND ENC TO MEGA        
+     //Serial.print("  cmd  ");Serial.print(cmd0Pos);
+     //Serial.print("  rS  ");Serial.print(rS);
+     //Serial.print("  rH  ");Serial.print(rH);
+     //Serial.print("  rL  ");Serial.print(rL);
+     //Serial.print("  rF  ");Serial.println(rF);
+
+     }
+
+}
+
+
+double get_angle_from_enc() {
+
+    double result=-1*encoderPos*0.00694;//(double)360/(64*810*1);
+    return result;
+}
+
+
+
+void send_cmd_to_motor(int cmdpwm) {
+
     int vPlus=0,vMinus=0;
-    if(cmdPwm>=0)
+    if(cmdpwm>=0)
     {
-      vPlus=cmdPwm;
+      vPlus=cmdpwm;
       vMinus=0;
     }
-    else if(cmdPwm<0)
+    else if(cmdpwm<0)
      {
       vPlus=0;
-      vMinus=-cmdPwm;
+      vMinus=-cmdpwm;
     }   
     
      if(vPlus>=255)
@@ -113,33 +131,24 @@ void loop(){
     analogWrite(MotorPin1,vPlus);
     analogWrite(MotorPin0,vMinus);
  
-
-
-
-
-/////WRITE ENC to mega
-/*
-    x=encoder0Pos;  
-    char xS='{';
-    byte xH =highByte(x);
-    byte xL =lowByte(x);
-    char xF='}'; 
-    Serial.print(xS);Serial.write(xH);Serial.write(xL);Serial.write(xF);
-*/
-    
-    }
 }
 
+
+
+
+
+
+
 void doEncoder() {
-    // encoder0Pos++;
+    // encoderPos++;
     pinAState = digitalRead(2);
     pinBState = digitalRead(3);
 
     if (pinAState == 0 && pinBState == 0) {
     if (pinAStateOld == 1 && pinBStateOld == 0) // forward
-    encoder0Pos ++;
+    encoderPos ++;
     if (pinAStateOld == 0 && pinBStateOld == 1) // reverse
-    encoder0Pos --;
+    encoderPos --;
     if (pinAStateOld == 1 && pinBStateOld == 1) // unknown
     unknownvalue ++;
     if (pinAStateOld == 0 && pinBStateOld == 0) // unknown
@@ -147,9 +156,9 @@ void doEncoder() {
     }
     if (pinAState == 0 && pinBState == 1) {
     if (pinAStateOld == 0 && pinBStateOld == 0) // forward
-    encoder0Pos ++;
+    encoderPos ++;
     if (pinAStateOld == 1 && pinBStateOld == 1) // reverse
-    encoder0Pos --;
+    encoderPos --;
     if (pinAStateOld == 1 && pinBStateOld == 0) // unknown
     unknownvalue ++;
     if (pinAStateOld == 0 && pinBStateOld == 1) // unknown
@@ -157,9 +166,9 @@ void doEncoder() {
     }
     if (pinAState == 1 && pinBState == 1) {
     if (pinAStateOld == 0 && pinBStateOld == 1) // forward
-    encoder0Pos ++;
+    encoderPos ++;
     if (pinAStateOld == 1 && pinBStateOld == 0) // reverse
-    encoder0Pos --;
+    encoderPos --;
     if (pinAStateOld == 0 && pinBStateOld == 0) // unknown
     unknownvalue ++;
     if (pinAStateOld == 1 && pinBStateOld == 1) // unknown
@@ -168,9 +177,9 @@ void doEncoder() {
 
     if (pinAState == 1 && pinBState == 0) {
     if (pinAStateOld == 1 && pinBStateOld == 1) // forward
-    encoder0Pos ++;
+    encoderPos ++;
     if (pinAStateOld == 0 && pinBStateOld == 0) // reverse
-    encoder0Pos --;
+    encoderPos --;
     if (pinAStateOld == 0 && pinBStateOld == 1) // unknown
     unknownvalue ++;
     if (pinAStateOld == 1 && pinBStateOld == 0) // unknown
@@ -182,7 +191,7 @@ void doEncoder() {
 
 
 /*
-Serial.print("Encoder: "); Serial.print(encoder0Pos); 
+Serial.print("Encoder: "); Serial.print(encoderPos); 
 Serial.print(" unKnown: ");Serial.print(unknownvalue, DEC); 
 Serial.println(""); 
 */
