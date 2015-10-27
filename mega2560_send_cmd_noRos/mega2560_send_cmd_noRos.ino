417e853a507dd49c455ee505feb0e010e5e164d4
ChingHengWang <qoogood1234@gmail.com>
2015-10-27 20:51:26 +0800

    modify

--- mega2560_send_cmd_noRos/mega2560_send_cmd_noRos.ino
+++ mega2560_send_cmd_noRos/mega2560_send_cmd_noRos.ino
@@ -9,7 +9,8 @@ char cS=0;
 byte cH=0;
 byte cL=0;
 byte cF=0;
-int cmd_P=-205;
+int cmd_P=200;
+int HomeValue=0;
 void setup() { 
 
  Serial.begin (115200);
@@ -24,17 +25,29 @@ void loop(){
       
 	/////WRITE CMD to UNO
 
-	    char cS='{';
-	    byte cH =highByte(cmd_P);
-	    byte cL =lowByte(cmd_P);
-	    char cF='}'; 
-	    mySerial.print(cS);mySerial.write(cH);mySerial.write(cL);mySerial.write(cF);
-      
+	  char cS='{';
+	  byte cH =highByte(cmd_P);
+	  byte cL =lowByte(cmd_P);
+	  char cF='}'; 
+	  mySerial.print(cS);mySerial.write(cH);mySerial.write(cL);mySerial.write(cF);
 
-
+   
+    char rS=(char)mySerial.read();
+    if(rS=='{'){
+    char commandArray[3];
+    mySerial.readBytes(commandArray,3);
+    byte rH=commandArray[0];
+    byte rL=commandArray[1];
+    char rF=commandArray[2];
+    if(rF=='}')         
+      HomeValue=(rH<<8)+rL; 
+    }
+    Serial.println(HomeValue);
   }
 
 }
+
+
 
 
 
