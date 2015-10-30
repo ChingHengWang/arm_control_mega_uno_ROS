8d1dad75b387c36a53e8ca3acde9e6b4090f2327
ChingHengWang <qoogood1234@gmail.com>
2015-10-30 16:32:31 +0800

    modify 2560_ROS

--- mega2560_send_cmd_Ros/mega2560_send_cmd_Ros.ino
+++ mega2560_send_cmd_Ros/mega2560_send_cmd_Ros.ino
@@ -6,7 +6,8 @@
 #include <Metro.h>
 
 ros::NodeHandle nh;
-SoftwareSerial mySerial(10, 11); // RX, TX
+SoftwareSerial mySerial_0(10, 11); // RX, TX
+SoftwareSerial mySerial_1(12, 13); // RX, TX
 
 //cmd Byte to UNO
 char commandArray[3];
@@ -14,20 +15,32 @@ char cS=0;
 byte cH=0;
 byte cL=0;
 byte cF=0;
-int cmd_P=0;
-void messageCb(const std_msgs::Int16& msg)
+int cmd_P_0=0;
+int cmd_P_1=0;
+
+void messageCallBack0(const std_msgs::Int16& msg)
 {
-    cmd_P=msg.data;
+    cmd_P_0=msg.data;
 	
 }
-ros::Subscriber<std_msgs::Int16> sub("cmd_angle",messageCb);
-     
+void messageCallBack1(const std_msgs::Int16& msg)
+{
+    cmd_P_1=msg.data;
+	
+}
+
+
+ros::Subscriber<std_msgs::Int16> sub_0("cmd_angle_0",messageCallBack0);
+ros::Subscriber<std_msgs::Int16> sub_1("cmd_angle_1",messageCallBack1);
+    
 Metro mainTimer = Metro(10);
 
 void setup() { 
  nh.initNode();
- nh.subscribe(sub);
- mySerial.begin(57600);
+ nh.subscribe(sub_0);
+ nh.subscribe(sub_1);
+ mySerial_0.begin(57600);
+ mySerial_1.begin(57600);
 
 } 
 
@@ -36,10 +49,10 @@ void loop(){
     ////WRITE CMD to UNO
 
     char cS='{';
-    byte cH =highByte(cmd_P);
-    byte cL =lowByte(cmd_P);
+    byte cH =highByte(cmd_P_0);
+    byte cL =lowByte(cmd_P_0);
     char cF='}'; 
-    mySerial.print(cS);mySerial.write(cH);mySerial.write(cL);mySerial.write(cF);
+    mySerial_0.print(cS);mySerial_0.write(cH);mySerial_0.write(cL);mySerial_0.write(cF);
 
     nh.spinOnce();
    
